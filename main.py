82f4f00869d423b6f480bbe6717b72988a0eb2f7
Du <PaulKatyunin@yandex.ru>
2021-01-03 18:44:02 +0300

    сделаль

--- main.py
+++ main.py
@@ -5,12 +5,33 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QPen, QColor
 from random import randint
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Window(QWidget):
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(756, 580)
+        Form.setStyleSheet("background-color: rgb(0, 0, 0);")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(340, 260, 93, 28))
+        self.pushButton.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Git и желтые окружности"))
+        self.pushButton.setText(_translate("Form", "Кнопка"))
+
+
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.do_paint = False
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
 
     def paint(self):
@@ -18,8 +39,8 @@ class Window(QWidget):
         self.repaint()
 
     def draw(self, qp):
-        qp.setPen(QColor("yellow"))
         for i in range(randint(1, 7)):
+            qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             d = randint(2, 250)
             qp.drawEllipse(randint(0, 700), randint(0, 500), d, d)
 
